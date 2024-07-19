f1540b80f61c8fd7a325d127d592f231689ec107
Lee Tze Cheun <tzecheun.lee@gmail.com>
2024-07-19 16:46:58 +0800

    Added helper functions to handle images encoded as Base64 string.

--- utils.py
+++ utils.py
@@ -5,37 +5,12 @@ here to avoid cluttering the notebook.
 
 """
 
+from os import PathLike
+
 import base64
 import json
 
-from unstructured.documents.elements import Element
-
-
-def categorize_elements(
-    raw_pdf_elements: list[Element]
-) -> tuple[list[str], list[str]]:
-    """
-    Categorize partitioned elements from 
-    a PDF document into tables and texts.
-    
-    Args:
-        raw_pdf_elements: List of elements partitioned from PDF.
-
-    Returns:
-        Tuple containing the text elements' 
-        contents and the table elements' contents.
-
-    """
-    texts, tables = ([], [])
-    
-    for element in raw_pdf_elements:
-        if element.category == "Table":
-            tables.append(element.text)
-        elif element.category == "CompositeElement":
-            texts.append(element.text)
-            
-    return texts, tables
-
+from IPython.display import HTML, display
 
 def write_to_json(summaries: list[str], json_path: str):
     """
@@ -74,3 +49,38 @@ def read_from_json(json_path: str) -> list[str]:
         encoding='utf-8') as f:
         return json.load(f)
     
+
+def encode_image(
+    image_path: PathLike
+) -> str:
+    """
+    Encodes an image as a Base64 string.
+    
+    Args:
+        image_path: Path to the image.
+
+    Returns:
+        A Base64 string.
+        
+    """
+    with open(
+        file=image_path, 
+        mode="rb" # read + binary mode
+    ) as image_file:
+        return base64.b64encode(
+            image_file.read()
+        ).decode("utf-8")
+
+
+def plt_img_base64(image_base64: str):
+    """
+    Display base64 encoded string as image
+
+    """
+    # Create an HTML img tag with the base64 string as the source
+    image_html = f'<img src="data:image/jpeg;base64,{image_base64}" />'
+
+    # Display the image by rendering the HTML
+    display(
+        HTML(image_html)
+    )
